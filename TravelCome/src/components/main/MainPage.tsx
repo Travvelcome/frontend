5e345a3daafb96122d104bb589a29cd0f2e88d9f
Yejin <kyj5840kyj@duksung.ac.kr>
2024-08-17 21:21:58 +0900

    feat: 폰트 및 라우터 설정

---
+++ TravelCome/src/components/main/MainPage.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const MainPage = () => {
+  return <Container>안녕하세요</Container>;
+};
+export default MainPage;
+
+const Container = styled.div`
+  font-family: "SanTokki";
+  font-size: 50px;
+`;
