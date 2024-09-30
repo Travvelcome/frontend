99654c0a767b44c4e08c04d188bb2ea33e4e6a81
Yejin <kyj5840kyj@duksung.ac.kr>
2024-09-30 16:26:16 +0900

    feat: 대화창 페이지 일부 구현

---
+++ TravelCome/src/components/talking/TalkingChattingPage.tsx
@@ -0,0 +1,195 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { RiHeadphoneFill } from "react-icons/ri";
+import { IoIosArrowBack } from "react-icons/io";
+
+const TalkingChattingPage = () => {
+  const navigate = useNavigate();
+
+  // 검색어
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const handleInputChange = (e: any) => {
+    const keyword = e.target.value;
+    setSearchKeyword(keyword);
+  };
+
+  return (
+    <Container>
+      <Background>
+        무엇이든 대화 나눠요!
+        <br />
+        질문이 아니어도 괜찮아요.
+      </Background>
+      <TitleBox>
+        <BackBtn
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <IoIosArrowBack />
+        </BackBtn>
+        <Title>용두암</Title>
+        <hr />
+      </TitleBox>
+      <ChattingBox></ChattingBox>
+      <QuestionBox></QuestionBox>
+      <SearchBox>
+        <SearchBar>
+          <SearchInput
+            id="input"
+            type="text"
+            autoFocus
+            placeholder="Message"
+            value={searchKeyword}
+            onChange={handleInputChange}
+          />
+        </SearchBar>
+        <div id="voice-icon">
+          <RiHeadphoneFill size="30" />
+        </div>
+      </SearchBox>
+    </Container>
+  );
+};
+export default TalkingChattingPage;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+const Background = styled.div`
+  font-family: "JejuGothic";
+  width: 160px;
+  text-align: center;
+  position: fixed;
+  top: 40%;
+  left: 31%;
+  font-size: 14px;
+  color: rgb(135, 136, 141, 0.5);
+  z-index: -999;
+`;
+const TitleBox = styled.div`
+  font-family: "SanTokki";
+  position: relative;
+  padding: 25px;
+  padding-bottom: 0px;
+  //border: 1px solid #000;
+`;
+const BackBtn = styled.div`
+  width: 40px;
+  height: 40px;
+  font-size: 30px;
+  color: #474751;
+  position: absolute;
+  top: 20px;
+  left: 25px;
+  z-index: 999;
+  cursor: pointer;
+`;
+const Title = styled.div`
+  width: 120px;
+  height: 25px;
+  line-height: 25px;
+  font-size: 28px;
+  color: #ff6b00;
+  margin: 0px auto;
+  padding-left: 50px;
+  padding-bottom: 10px;
+`;
+const ChattingBox = styled.div`
+  font-family: "JejuGothic";
+  position: relative;
+  padding: 10px;
+  //border: 1px solid #111;
+`;
+const Date = styled.div`
+  font-family: "JejuGothic";
+  font-size: 14px;
+  color: #87888d;
+  position: relative;
+  text-align: center;
+`;
+const Day = styled.div`
+  padding: 15px 0;
+  //border: 1px solid #000;
+`;
+const AI = styled.div`
+  width: max-content;
+  max-width: 250px;
+  height: max-content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+  padding: 15px 25px;
+  font-family: "JejuGothic";
+  font-size: 14px;
+  border-radius: 30px;
+  color: #000;
+  background-color: rgb(255, 107, 0, 0.4);
+`;
+const Me = styled.div`
+  width: max-content;
+  max-width: 250px;
+  height: max-content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+  margin-left: auto; // 오른쪽 정렬
+  padding: 15px 25px;
+  font-family: "JejuGothic";
+  font-size: 14px;
+  border-radius: 30px;
+  color: #fff;
+  background-color: rgb(255, 107, 0, 0.8);
+`;
+const QuestionBox = styled.div`
+  width: 100%;
+  height: 100px;
+  font-family: "JejuGothic";
+  position: absolute;
+  bottom: 85px;
+  border: 1px solid #111;
+`;
+const SearchBox = styled.div`
+  font-family: "JejuGothic";
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  padding: 20px 10px;
+  padding-right: 60px;
+  //border: 1px solid #111;
+
+  #voice-icon {
+    color: #111;
+    cursor: pointer;
+    display: inline-block;
+    position: absolute;
+    top: 25px;
+    right: 10px;
+    cursor: pointer;
+  }
+`;
+const SearchBar = styled.div`
+  font-family: "JejuGothic";
+  position: relative;
+  width: 310px;
+  height: 44px;
+  border-radius: 30px;
+  border: 1px solid #000;
+  display: inline-block;
+`;
+const SearchInput = styled.input`
+  width: 250px;
+  height: 40px;
+  border: none;
+  margin-left: 20px;
+  font-size: 20px;
+  color: #333;
+  &:focus {
+    outline: none;
+  }
+`;
---
+++ TravelCome/src/components/talking/TalkingMainPage.tsx
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { IoSearchSharp } from "react-icons/io5";
+import { IoIosArrowBack } from "react-icons/io";
+import { ReactComponent as TalkingBtn } from "../../assets/TalkingBtn.svg";
+import RecommendList from "./TalkingRecommendList";
+
+const TalkingMainPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <TitleBox>
+        <BackBtn
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <IoIosArrowBack />
+        </BackBtn>
+        <Title>새로운 장소 발견!</Title>
+      </TitleBox>
+      <ImageBox></ImageBox>
+      <InfoBox>
+        <InfoTitleBox>
+          <InfoTitle>용두암</InfoTitle>
+          <InfoAddress>제주도 제주시 용담동</InfoAddress>
+          <span>
+            <TalkingBtn
+              onClick={() => {
+                navigate("/frontend/talking/chatting");
+              }}
+            />
+          </span>
+        </InfoTitleBox>
+        <InfoDetailBox>
+          제주의 용연과 용두암은 용의 전설을 간직한 아름다운 경승지로, 용연은
+          바다와 연못이 어우러진 자연경관으로 여름에는 용연야범 축제로 유명하며,
+          용두암은 용의 머리를 상징하는 바위로 두 가지 전설을 담고 있습니다.
+          이곳은 데이터 장소로 사랑받으며, 애월 해안도로에서의 드라이브 코스도
+          인기가 높습니다.
+        </InfoDetailBox>
+        <InfoCategoryBox>
+          <Category>🌊️ 바다</Category>
+          <Category>🐬 생태 / 자연환경</Category>
+          <Category>🐉 신화 / 전통</Category>
+        </InfoCategoryBox>
+      </InfoBox>
+      <RecommendBox>
+        <RecommendTitle>민지님의 취향 주제</RecommendTitle>
+        <RecommendListBox>
+          <RecommendList />
+          <RecommendList />
+          <RecommendList />
+          <RecommendList />
+        </RecommendListBox>
+      </RecommendBox>
+    </Container>
+  );
+};
+export default TalkingMainPage;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+const TitleBox = styled.div`
+  font-family: "JejuGothic";
+  position: relative;
+  padding: 25px;
+  padding-bottom: 0px;
+`;
+const BackBtn = styled.div`
+  width: 40px;
+  height: 40px;
+  font-size: 30px;
+  color: #474751;
+  position: absolute;
+  top: 20px;
+  left: 25px;
+  z-index: 999;
+  cursor: pointer;
+`;
+const Title = styled.div`
+  width: 150px;
+  height: 25px;
+  line-height: 20px;
+  font-size: 20px;
+  color: #ff6b00;
+  margin: 0px auto;
+  padding-bottom: 10px;
+`;
+const ImageBox = styled.div`
+  width: 100vw;
+  height: 180px;
+  background-color: #d9d9d9;
+`;
+
+const InfoBox = styled.div`
+  font-family: "JejuGothic";
+  position: relative;
+  padding: 25px;
+  padding-bottom: 0px;
+  //border: 1px solid #000;
+`;
+const InfoTitleBox = styled.div`
+  font-family: "JejuGothic";
+  position: relative;
+  padding: 0px 25px;
+  padding-bottom: 10px;
+
+  span {
+    position: absolute;
+    top: 0px;
+    right: 10px;
+    cursor: pointer;
+  }
+`;
+const InfoTitle = styled.div`
+  font-family: "SanTokki";
+  font-size: 34px;
+  margin: 10px 0;
+`;
+const InfoAddress = styled.div`
+  font-size: 12px;
+  color: #87888d;
+`;
+const InfoDetailBox = styled.div`
+  font-family: "JejuGothic";
+  font-size: 14px;
+  line-height: 18px;
+  position: relative;
+  padding: 10px 15px;
+  //border: 1px solid #000;
+`;
+const InfoCategoryBox = styled.div`
+  font-family: "JejuGothic";
+  font-size: 14px;
+  line-height: 18px;
+  position: relative;
+  padding: 10px 15px;
+  //border: 1px solid #000;
+`;
+const Category = styled.span`
+  font-family: "JejuGothic";
+  font-size: 14px;
+  line-height: 18px;
+  padding: 0 5px;
+  margin: 5px 3px;
+  //border: 1px solid #111;
+  border-radius: 10px;
+  display: inline-block;
+`;
+
+const RecommendBox = styled.div`
+  font-family: "JejuGothic";
+  position: relative;
+  padding: 25px;
+  padding-bottom: 0px;
+  //border: 1px solid #000;
+`;
+const RecommendTitle = styled.div`
+  font-family: "SanTokki";
+  font-size: 18px;
+  margin: 10px 0;
+`;
+const RecommendListBox = styled.div`
+  width: 100%;
+  overflow: auto;
+  white-space: nowrap;
+`;
